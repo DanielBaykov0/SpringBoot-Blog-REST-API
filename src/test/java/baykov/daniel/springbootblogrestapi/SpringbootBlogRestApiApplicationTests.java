289a2ca425cca1209eedab8d32699353f3f6c949
Daniel Baykov <danibaikov@gmail.com>
2023-06-16 08:22:48 +0300

    Initial commit

---
+++ src/test/java/baykov/daniel/springbootblogrestapi/SpringbootBlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package baykov.daniel.springbootblogrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
