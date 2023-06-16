2ade4112249b32ea024e933c88d605d910d8a3c7
Daniel Baykov <danibaikov@gmail.com>
2023-06-16 17:45:33 +0300

    refactoring hardcoded values for Paging and Sorting

--- src/main/java/baykov/daniel/springbootblogrestapi/controllers/PostController.java
+++ src/main/java/baykov/daniel/springbootblogrestapi/controllers/PostController.java
@@ -3,6 +3,7 @@ package baykov.daniel.springbootblogrestapi.controllers;
 import baykov.daniel.springbootblogrestapi.payload.PostDto;
 import baykov.daniel.springbootblogrestapi.payload.PostResponse;
 import baykov.daniel.springbootblogrestapi.services.PostService;
+import baykov.daniel.springbootblogrestapi.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,10 +26,10 @@ public class PostController {
 
     // get all posts rest api
     @GetMapping
-    public PostResponse getAllPosts(@RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
-                                    @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
-                                    @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
-                                    @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir) {
+    public PostResponse getAllPosts(@RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+                                    @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+                                    @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY_ID, required = false) String sortBy,
+                                    @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIR_ASC, required = false) String sortDir) {
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
---
+++ src/main/java/baykov/daniel/springbootblogrestapi/utils/AppConstants.java
@@ -0,0 +1,9 @@
+package baykov.daniel.springbootblogrestapi.utils;
+
+public class AppConstants {
+
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_PAGE_SIZE = "10";
+    public static final String DEFAULT_SORT_BY_ID = "id";
+    public static final String DEFAULT_SORT_DIR_ASC = "asc";
+}
