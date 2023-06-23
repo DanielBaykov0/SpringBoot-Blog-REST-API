1679b81c5e311999935c1c7d21c3b5da0242aa98
Daniel Baykov <danibaikov@gmail.com>
2023-06-23 21:32:32 +0300

    validate Create Post and Update Post REST API Request

--- src/main/java/baykov/daniel/springbootblogrestapi/controllers/PostController.java
+++ src/main/java/baykov/daniel/springbootblogrestapi/controllers/PostController.java
@@ -4,6 +4,7 @@ import baykov.daniel.springbootblogrestapi.payload.PostDto;
 import baykov.daniel.springbootblogrestapi.payload.PostResponse;
 import baykov.daniel.springbootblogrestapi.services.PostService;
 import baykov.daniel.springbootblogrestapi.utils.AppConstants;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,7 @@ public class PostController {
 
     // create blog post
     @PostMapping
-    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+    public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
@@ -41,7 +42,7 @@ public class PostController {
 
     // update post by id
     @PutMapping("/{id}")
-    public ResponseEntity<PostDto> updatePostById(@RequestBody PostDto postDto, @PathVariable(name = "id") long id) {
+    public ResponseEntity<PostDto> updatePostById(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") long id) {
         PostDto postResponse = postService.updatePostById(postDto, id);
         return new ResponseEntity<>(postResponse, HttpStatus.OK);
     }
