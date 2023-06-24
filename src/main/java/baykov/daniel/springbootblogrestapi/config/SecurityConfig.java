13784658b5944f3abebb321dc38aa5785b3c4438
Daniel Baykov <danibaikov@gmail.com>
2023-06-24 13:43:53 +0300

    securing REST API's with Database Authentication

--- src/main/java/baykov/daniel/springbootblogrestapi/config/SecurityConfig.java
+++ src/main/java/baykov/daniel/springbootblogrestapi/config/SecurityConfig.java
@@ -3,25 +3,35 @@ package baykov.daniel.springbootblogrestapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    private UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -34,10 +44,10 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("admin")).roles("admin").build();
-        UserDetails daniel = User.builder().username("daniel").password(passwordEncoder().encode("baykov")).roles("user").build();
-        return new InMemoryUserDetailsManager(admin, daniel);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("admin")).roles("admin").build();
+//        UserDetails daniel = User.builder().username("daniel").password(passwordEncoder().encode("baykov")).roles("user").build();
+//        return new InMemoryUserDetailsManager(admin, daniel);
+//    }
 }
